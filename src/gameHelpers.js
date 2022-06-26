9f8594c4e96e637177234020ee2395f1df61d674
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-06-26 15:53:11 -0600

    fix: fix bug of blank piece appearing if duplicate pieces are randomly selected

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -5,3 +5,20 @@ export const createStage = () =>
   Array.from(Array(STAGE_HEIGHT), () =>
    new Array(STAGE_WIDTH).fill([0, 'clear'])
   ) 
+
+export const checkCollision = (player, stage, { x: moveX, y: moveY }) => {
+  for (let y= 0; y < player.tetromino.length; y =+ 1) {
+    for (let x = 0; x < player.tetromino[y].length; x =+ 1) {
+
+      //1.check that tetromino cell isn't 0
+      if (player.tetromino[x][y] !== 0) {
+        // 2. check move is inside game area height y, shouldnt go through bottom
+        !stage[y + player.pos.y + moveY]
+        // 3. check piece isnt moving outside of width x
+      
+        // 4. check cell that moving to isn't set to clear
+      }
+ 
+    }
+  }
+}
--- src/tetrominos.js
+++ src/tetrominos.js
@@ -49,7 +49,7 @@ export const TETROMINOS = {
     shape: [
       [0, 0, 0],
       ['T', 'T', 'T'],
-      [0, 'T', 'T'],
+      [0, 'T', 0],
     ],
     color: '132, 61, 198',
   },
